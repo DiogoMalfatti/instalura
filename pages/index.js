8e0749aafa3b71ec3c0ae6ee22a8fc5b1cd3eb9d
Diogo Malfatti <diimalfatti@gmail.com>
2021-02-13 09:31:04 -0300

    created reset-css/footer

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Menu from '../src/components/commons/Menu'
+import Footer from '../src/components/commons/Footer'
 
 export default function Home() {
 
     return (
-        <div>
+        <div style={{
+            flex: '1',
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        }}>
             <Menu />
+            <Footer />
         </div>
     )
 }
--- src/theme/index.js
+++ src/theme/index.js
@@ -2,47 +2,48 @@ import React from 'react'
 
 const colors = {
     background: {
-      light: {
-        color: '#FFFFFF',
-      },
-      main: {
-        color: '#F2F2F2',
-      },
+        light: {
+            color: '#FFFFFF',
+        },
+        main: {
+            color: '#F2F2F2',
+        },
     },
     borders: {
-      main: {
-        color: '#F1F1F1',
-      },
+        main: {
+            color: '#F1F1F1',
+        },
     },
     primary: {
-      main: {
-        color: '#D7385E',
-        contrastText: '#fff',
-      },
+        main: {
+            color: '#D7385E',
+            contrastText: '#fff',
+        },
     },
     secondary: {
-      main: {
-        color: '#FB7B6B',
-        contrastText: '#fff',
-      },
+        main: {
+            color: '#FB7B6B',
+            contrastText: '#fff',
+        },
     },
     tertiary: {
-      main: {
-        color: '#070C0E',
-        contrastText: '#fff',
-      },
-      light: {
-        color: '#88989E',
-        contrastText: '#fff',
-      },
+        main: {
+            color: '#070C0E',
+            contrastText: '#fff',
+        },
+        light: {
+            color: '#88989E',
+            contrastText: '#fff',
+        },
     },
     modes: {
-      dark: {},
+        dark: {},
     },
-  };
+};
   
-  export default {
+export default {
     colors,
     borderRadius: '8px',
+    fontFamily: '\'Rubik\', sans-serif',
     transition: '200ms ease-in-out',
-  };
+};
