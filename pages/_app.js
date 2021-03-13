f7be8ea74f0272fe3f192a36deb23bc41248e747
Diogo Malfatti <diimalfatti@gmail.com>
2021-03-12 21:07:17 -0300

    tested

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import theme from '../src/theme';
 import { GlobalStyle } from '../src/theme/GlobalStyle';
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
